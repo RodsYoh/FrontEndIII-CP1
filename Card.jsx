6166afb63da07e1a38c430b1960073a4cfecf22e
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2022-11-07 23:23:49 -0300

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,12 @@
+//import './style.scss'
+
+export function Card(props) {
+
+    return (
+        <div className="Card" /*style={{background-color:"${props.card.cor}"}}*/>
+            <div className="card-body">
+                <h1>{props.card.nome}</h1>
+            </div>
+        </div>
+    )
+}
