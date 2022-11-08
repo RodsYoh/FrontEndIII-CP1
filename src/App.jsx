a7888a09444d887e6c40c35916ad8bca421ba2fe
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2022-11-07 23:12:20 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react'
+import './style.scss'
+import { Card } from './Card'
+export function checkPoint() {
+
+    const [novoNome, setNovoNome] = useState('')
+    const [novaCor, setNovaCor] = useState('')
+    const [formularioErro, setFormularioErro] = useState(false)
+    const [listaCores, setListaCores] = useState([
+        {
+            nome: 'Rodrigo Araújo',
+            cor: ' 	#FFDAB9'
+        },
+        {
+            nome: 'Matheus Almeida',
+            cor: '#00BFFF'
+        },
+        {
+            nome: 'Maiara Pontes',
+            cor: '#EE82EE'
+        }
+    ])
+
+    function novaCorLista(event) {
+
+        event.preventDefault()
+
+        const novaCor = {
+            nome: novoNome,
+            cor: novaCor
+        }
+
+        if (novoNome === '' || novaCor === '') {
+
+            setFormularioErro(true)
+
+        } else {
+
+            setFormularioErro(false)
+
+            setListaCores([...listaCores, novaCor])
+
+            setNovoNome('')
+            setNovaCor('')
+        }
+
+        console.log(opcao)
+
+    }
+
+    return (
+
+        <main className="setima-aula-component">
+
+            <div className="tittle-wrapper">
+                <h1>Produtos</h1>
+                {/* <button onClick={addNewProduct}>Adicionar novo produto</button> */}
+            </div>
+
+            <form className={formularioErro ? 'form-error' : ''} onSubmit={event => novaCorLista(event)}>
+
+                <div>
+                    <label htmlFor="novoNome">Nome</label>
+                    <input id="novoNome" type="text" value={novoNome} onChange={event => setNovoNome(event.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="novaCor">Foto</label>
+                    <input id="novaCor" type="color" value={novaCor} onChange={event => setNovaCor(event.target.value)} />
+                </div>
+
+                <button type='submit'>Cadastrar cor</button>
+            </form>
+
+            {
+                formularioErro ? (
+                    <span>Preencha todos os campos</span>
+                ) : null
+            }
+
+            <section className='products'>
+                {
+                    listaCores.map(
+                        cor => {
+                            return (
+                                <Card
+                                    cor={cor}
+                                />
+                            )
+                        }
+                    )
+                }
+            </section>
+
+        </main>
+
+    )
+
+}
