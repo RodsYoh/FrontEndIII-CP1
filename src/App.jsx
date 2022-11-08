a84b0057203208c2c8ea3264fc32de4222a5e766
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2022-11-08 11:23:16 -0300

    Diversas mudanças, pelo menos aparece a tela agora...

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import './style.scss'
+import './style.css'
 import { Card } from './Card'
-export function checkPoint() {
+
+function App() {
 
     const [novoNome, setNovoNome] = useState('')
     const [novaCor, setNovaCor] = useState('')
     const [formularioErro, setFormularioErro] = useState(false)
+    
     const [listaCores, setListaCores] = useState([
-        {
-            nome: 'Rodrigo Araújo',
-            cor: ' 	#FFDAB9'
-        },
-        {
-            nome: 'Matheus Almeida',
-            cor: '#00BFFF'
-        },
-        {
-            nome: 'Maiara Pontes',
-            cor: '#EE82EE'
-        }
+        // {
+        //     novoNome: 'Rodrigo Araújo',
+        //     novaCor: ' 	#FFDAB9'
+        // },
+        // {
+        //     novoNome: 'Matheus Almeida',
+        //     novaCor: '#00BFFF'
+        // },
+        // {
+        //     novoNome: 'Maiara Pontes',
+        //     novaCor: '#EE82EE'
+        // }
     ])
 
-    function novaCorLista(event) {
+    function cadastroCor(event) {
 
         event.preventDefault()
 
-        const novaCor = {
-            nome: novoNome,
-            cor: novaCor
+        const novoCadastroCor = {
+
         }
 
         if (novoNome === '' || novaCor === '') {
@@ -38,48 +39,37 @@ export function checkPoint() {
 
             setFormularioErro(false)
 
-            setListaCores([...listaCores, novaCor])
+            setListaCores([...listaCores, novoCadastroCor])
 
             setNovoNome('')
             setNovaCor('')
         }
-
-        console.log(opcao)
-
     }
 
     return (
-
-        <main className="setima-aula-component">
+        <html lang="en">
+            <main className="checkpoint">
 
             <div className="tittle-wrapper">
-                <h1>Produtos</h1>
-                {/* <button onClick={addNewProduct}>Adicionar novo produto</button> */}
+                <h1>Cores Favoritas</h1>
             </div>
-
-            <form className={formularioErro ? 'form-error' : ''} onSubmit={event => novaCorLista(event)}>
-
+            <form className={formularioErro ? 'form-error' : ''} onSubmit={event => cadastroCor(event)}>
+                <div className='dados'>
                 <div>
                     <label htmlFor="novoNome">Nome</label>
-                    <input id="novoNome" type="text" value={novoNome} onChange={event => setNovoNome(event.target.value)} />
+                    <input id="novoNome" name="novoNome" type="text" value={novoNome} placeholder="Preencha o nome" onChange={event => setNovoNome(event.target.value)} />
                 </div>
 
                 <div>
-                    <label htmlFor="novaCor">Foto</label>
-                    <input id="novaCor" type="color" value={novaCor} onChange={event => setNovaCor(event.target.value)} />
+                    <label htmlFor="novaCor">Cor</label>
+                    <input id="novaCor" name="novaCor" type="color" value={novaCor} onChange={event => setNovaCor(event.target.value)} />
                 </div>
-
+                </div>
                 <button type='submit'>Cadastrar cor</button>
             </form>
-
+            <h1>Lista de Cores</h1>
+            <section>
             {
-                formularioErro ? (
-                    <span>Preencha todos os campos</span>
-                ) : null
-            }
-
-            <section className='products'>
-                {
                     listaCores.map(
                         cor => {
                             return (
@@ -92,8 +82,15 @@ export function checkPoint() {
                 }
             </section>
 
+            {
+                formularioErro ? (
+                    <span>Preencha todos os campos</span>
+                ) : null
+            }
+
         </main>
-
+    </html>
     )
-
 }
+
+export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
-import './style.scss'
+import './style.css'
 
 export function Card(props) {
 
     return (
-        <div className="Card" style={{background-color:"${props.card.cor}"}}>
+        <div className="Card" style={{backgroundColor:props.card.novaCor}}>
             <div className="card-body">
-                <h1>{props.card.nome}</h1>
+                <h1>{props.card.novoNome}</h1>
+                <p>{props.card.novaCor}</p>
             </div>
         </div>
     )
