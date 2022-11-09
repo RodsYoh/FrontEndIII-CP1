fa02d91796029325e6abc620f13f9d52acafdd06
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2022-11-09 13:11:38 -0300

    09/11

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 import './style.css'
 
 export function Card(props) {
-
+const {cor} = props
     return (
-        <div className="Card" style={{backgroundColor:props.card.novaCor}}>
+        <div className="Card" style={{backgroundColor:cor.novaCor}}>
             <div className="card-body">
-                <h1>{props.card.novoNome}</h1>
-                <p>{props.card.novaCor}</p>
+                <h1>{cor.novoNome}</h1>
+                <p>{cor.novaCor}</p>
             </div>
         </div>
     )
