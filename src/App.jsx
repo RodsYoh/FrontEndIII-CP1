4d1e0c9cab949ecdc8759ea82ac15131b6265798
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2022-11-09 21:39:20 -0300

    Adicionada regra no nome (3 char)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const [novoNome, setNovoNome] = useState('')
     const [novaCor, setNovaCor] = useState('')
     const [formularioErro, setFormularioErro] = useState(false)
-    
+
     const [listaCores, setListaCores] = useState([
         {
             novoNome: 'Pêssego',
@@ -30,9 +30,9 @@ function App() {
         const novoCadastroCor = {
             novaCor: novaCor,
             novoNome: novoNome
-    }
+        }
 
-        if (novoNome === '' || novaCor === '') {
+        if (novoNome === '' || novaCor === '' || novoNome.length<3) {
 
             setFormularioErro(true)
 
@@ -51,44 +51,44 @@ function App() {
         <html lang="en" className="color-change-5x">
             <main className="checkpoint">
 
-            <div className="tittle-wrapper">
-                <h1>Cores Favoritas</h1>
-            </div>
-            <form className={formularioErro ? 'form-error' : ''} onSubmit={event => cadastroCor(event)}>
-                <div className='dados'>
-                <div>
-                    <label htmlFor="novoNome">Nome da cor:  </label>
-                    <input id="novoNome" name="novoNome" type="text" value={novoNome.trim()} placeholder="Preencha o nome" onChange={event => setNovoNome(event.target.value)} />
+                <div className="tittle-wrapper">
+                    <h1>Cores Favoritas</h1>
                 </div>
+                <form className={formularioErro ? 'form-error' : ''} onSubmit={event => cadastroCor(event)}>
+                    <div className='dados'>
+                        <div>
+                            <label htmlFor="novoNome">Nome da cor:  </label>
+                            <input id="novoNome" name="novoNome" type="text" value={novoNome.trim()} placeholder='Nome da cor (min. 3 letras).' onChange={event => setNovoNome(event.target.value)} />
+                        </div>
 
-                <div>
-                    <label htmlFor="novaCor">Cor:  </label>
-                    <input id="novaCor" name="novaCor" type="color" value={novaCor} onChange={event => setNovaCor(event.target.value)} />
-                </div>
-                </div>
-                <button type='submit'>Cadastrar cor</button>
-            </form>
-            {
-                formularioErro ? (
-                    <span>Preencha todos os campos</span>
-                ) : null
-            }
-            <h1>Lista de Cores</h1>
-            <section class="listaDeCores">
-            {
-                    listaCores.map(
-                        cor => {
-                            return (
-                                <Card
-                                    cor={cor}
-                                />
-                            )
-                        }
-                    )
+                        <div>
+                            <label htmlFor="novaCor">Cor:  </label>
+                            <input id="novaCor" name="novaCor" type="color" value={novaCor.toUpperCase()} onChange={event => setNovaCor(event.target.value)} />
+                        </div>
+                    </div>
+                    <button type='submit'>Cadastrar cor</button>
+                </form>
+                {
+                    formularioErro ? (
+                        <span>Preencha corretamente os campos</span>
+                    ) : null
                 }
-            </section>
-        </main>
-    </html>
+                <h1>Lista de Cores</h1>
+                <section class="listaDeCores">
+                    {
+                        listaCores.map(
+                            cor => {
+                                return (
+                                    <Card
+                                        cor={cor}
+                                    />
+                                )
+                            }
+                        )
+                    }
+                </section>
+            </main>
+        </html>
     )
 }
 
